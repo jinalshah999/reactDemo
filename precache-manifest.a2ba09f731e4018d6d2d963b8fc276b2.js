ed2c94968a53aa7a9b2e02e718e4b92980ee4881
jinalshah999 <jinalshah999@gmail.com>
2020-03-14 17:01:37 +0530

    Updates

---
+++ precache-manifest.a2ba09f731e4018d6d2d963b8fc276b2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9d1949e43c51eefa562f9c19d0aeaca3",
+    "url": "/reactDemo/index.html"
+  },
+  {
+    "revision": "b06ad2339db4c04c8c1a",
+    "url": "/reactDemo/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "76625b77d881870c4ce1",
+    "url": "/reactDemo/static/js/2.57246241.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/reactDemo/static/js/2.57246241.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b06ad2339db4c04c8c1a",
+    "url": "/reactDemo/static/js/main.7c402797.chunk.js"
+  },
+  {
+    "revision": "5e1ab656183895438b93",
+    "url": "/reactDemo/static/js/runtime-main.c4602be5.js"
+  }
+]);
